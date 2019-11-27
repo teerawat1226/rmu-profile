faf11df5de3e7a1de7b2b16627fe273c74908c3b
Golf <golfzaza0512@gmail.com>
2019-11-27 21:40:09 +0700

    test

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,18 +6,21 @@ import { AppComponent } from './app.component';
 import { ProfileTeerawatComponent } from './profile/profile-teerawat/profile-teerawat.component';
 import { ProfilePanuwatComponent } from './profile/profile-panuwat/profile-panuwat.component';
 import { ProfileWeerapolComponent } from './profile/profile-weerapol/profile-weerapol.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     ProfileTeerawatComponent,
     ProfilePanuwatComponent,
-    ProfileWeerapolComponent
+    ProfileWeerapolComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
     RouterModule.forRoot([
       { path: '', redirectTo: '/profile', pathMatch: 'full' },
+      { path: 'profile', component: HomeComponent },
       { path: 'profile/Teerawat', component: ProfileTeerawatComponent },
       { path: 'profile/panuwat', component: ProfilePanuwatComponent },
       { path: 'profile/weerapol', component: ProfileWeerapolComponent },
